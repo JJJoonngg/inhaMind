f2edf950dd9f30622810c43ffdfa70224a922ec6
JJong <whdtls3878@naver.com>
2020-02-17 15:00:19 +0900

    기본 basic 틀 구현 완료

---
+++ app/src/main/java/com/example/inhamind/BoardFragment.java
@@ -0,0 +1,16 @@
+package com.example.inhamind;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class BoardFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.text_fragment2, container, false);
+    }
+}
--- app/src/main/java/com/example/inhamind/MainActivity.java
+++ app/src/main/java/com/example/inhamind/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.inhamind;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -23,10 +22,13 @@ public class MainActivity extends AppCompatActivity {
     private Context context = this;
 
     private FragmentManager fragmentManager = getSupportFragmentManager();
-    private FragmentTest1 fragmentTest1 = new FragmentTest1();
-    private FragmentTest2 fragmentTest2 = new FragmentTest2();
-    private FragmentTest3 fragmentTest3 = new FragmentTest3();
-    private FragmentTest4 fragmentTest4 = new FragmentTest4();
+    private HomeFragment homeFragment = new HomeFragment();
+    private BoardFragment boardFragment = new BoardFragment();
+    private MyHelpFragment myHelpFragment = new MyHelpFragment();
+    private ChattingFragment chattingFragment = new ChattingFragment();
+    private MyPageFragment mypageFragment = new MyPageFragment();
+    private NoticeFragment noticeFragment = new NoticeFragment();
+    private CustomerServiceFragment customerServiceFragment = new CustomerServiceFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.frameLayout, fragmentTest1).commitAllowingStateLoss();
+        transaction.replace(R.id.frameLayout, homeFragment).commitAllowingStateLoss();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
@@ -55,17 +57,17 @@ public class MainActivity extends AppCompatActivity {
                 menuItem.setChecked(true);
                 mDrawerLayout.closeDrawers();
 
-                int id = menuItem.getItemId();
-                String title = menuItem.getTitle().toString();
-
-                if (id == R.id.account) {
-                    Toast.makeText(context, title + ": 계정 정보를 확인합니다.", Toast.LENGTH_SHORT).show();
-                } else if (id == R.id.setting) {
-                    Toast.makeText(context, title + ": 설정 정보를 확인합니다.", Toast.LENGTH_SHORT).show();
-                } else if (id == R.id.logout) {
-                    Toast.makeText(context, title + ": 로그아웃 시도중", Toast.LENGTH_SHORT).show();
+                switch (menuItem.getItemId()) {
+                    case R.id.myPage:
+                        transaction.replace(R.id.frameLayout, mypageFragment).commitAllowingStateLoss();
+                        break;
+                    case R.id.notice:
+                        transaction.replace(R.id.frameLayout, noticeFragment).commitAllowingStateLoss();
+                        break;
+                    case R.id.customerService:
+                        transaction.replace(R.id.frameLayout, customerServiceFragment).commitAllowingStateLoss();
+                        break;
                 }
-
                 return true;
             }
         });
@@ -81,11 +83,12 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
     @Override
-    public void onBackPressed(){
-        if(mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
             mDrawerLayout.closeDrawer(GravityCompat.START);
-        }else{
+        } else {
             super.onBackPressed();
         }
     }
@@ -96,18 +99,17 @@ public class MainActivity extends AppCompatActivity {
             FragmentTransaction transaction = fragmentManager.beginTransaction();
 
             switch (menuItem.getItemId()) {
-                case R.id.test1:
-                    transaction.replace(R.id.frameLayout, fragmentTest1).commitAllowingStateLoss();
-
+                case R.id.home:
+                    transaction.replace(R.id.frameLayout, homeFragment).commitAllowingStateLoss();
                     break;
-                case R.id.test2:
-                    transaction.replace(R.id.frameLayout, fragmentTest2).commitAllowingStateLoss();
+                case R.id.board:
+                    transaction.replace(R.id.frameLayout, boardFragment).commitAllowingStateLoss();
                     break;
-                case R.id.test3:
-                    transaction.replace(R.id.frameLayout, fragmentTest3).commitAllowingStateLoss();
+                case R.id.myHelp:
+                    transaction.replace(R.id.frameLayout, myHelpFragment).commitAllowingStateLoss();
                     break;
-                case R.id.test4:
-                    transaction.replace(R.id.frameLayout, fragmentTest4).commitAllowingStateLoss();
+                case R.id.chatting:
+                    transaction.replace(R.id.frameLayout, chattingFragment).commitAllowingStateLoss();
                     break;
             }
             return true;
