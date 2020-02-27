024db1ae47360657208bbe44f99ba87fafa8ca09
JJong <whdtls3878@naver.com>
2020-02-27 20:20:26 +0900

    이메일 인증 부분 추가
    CountDownTimer 이용해서 인증코드 시간제한
    시간 만료시 코드 재생성을 통하여 이전코드를 못쓰게 하고
    Toastmsg 띄우도록함
    
    학번 8자리 미만 입력은 받지 않게 만들고
    인증이 확인 된다면
    학번, 인증코드, 입력부분과 버튼 부분 비활성화

--- app/src/main/java/com/example/inhamind/Account/makeAccountActivity.java
+++ app/src/main/java/com/example/inhamind/Account/makeAccountActivity.java
@@ -1,9 +1,6 @@
 package com.example.inhamind.Account;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -32,6 +29,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 
@@ -40,9 +38,6 @@ public class makeAccountActivity extends LoginActivity {
     View dialogLayout; //layout을 담을 View
     Dialog authDialog;
 
-    // final Random rand = new Random();
-    //        final TextView textGenerateNumber = (TextView)findViewById(R.id.generatenumber);
-    //        textGenerateNumber.setText(String.valueOf(rand.nextInt(9000)+1000));
     EditText name_input;
     EditText student_id_input;
     EditText confirm_num_input;
@@ -54,6 +49,7 @@ public class makeAccountActivity extends LoginActivity {
     TextView pswd_confirm;
     TextView count_view;
     TextView alert_messege;
+    TextView count_down;
 
     private Button btn;
     FirebaseAuth mAuth;
@@ -62,6 +58,7 @@ public class makeAccountActivity extends LoginActivity {
     private String authCode;
 
     final String FIRESTORE_TAG = "[FIRESTORE_TAG]";
+    final int Thousand = 1000;
     private int checkPwdLength = 0;
 
     @Override
@@ -78,6 +75,7 @@ public class makeAccountActivity extends LoginActivity {
 
         count_view = (TextView) findViewById(R.id.count_view);
         pswd_confirm = (TextView) findViewById(R.id.repswd_confirm);
+        count_down = (TextView) findViewById(R.id.count_down_time);
 
         pwd_join_confirm.addTextChangedListener(new TextWatcher() {
             @Override
@@ -125,9 +123,26 @@ public class makeAccountActivity extends LoginActivity {
         certification_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                authCode = "1";
-                MailSend mailSend = new MailSend(makeAccountActivity.this, student_id_input.getText().toString(), authCode);
-                mailSend.sendMail();
+                if (student_id_input.getText().toString().length() == 8) {
+                    authCode = createEmailCode();
+                    MailSend mailSend = new MailSend(makeAccountActivity.this, student_id_input.getText().toString(), authCode);
+                    mailSend.sendMail();
+                    CountDownTimer countDownTimer = new CountDownTimer(Thousand * 300, Thousand) {
+                        @Override
+                        public void onTick(long m) {
+                            count_down.setText(String.format(Locale.getDefault(), "%02d : %02d", (m / 1000L) / 60, (m / 1000L) % 60));
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            authCode = createEmailCode();
+                            count_down.setText("");
+                            Toast.makeText(makeAccountActivity.this, "인증코드를 재전송 해주세요.", Toast.LENGTH_SHORT).show();
+                        }
+                    }.start();
+                }
+                else
+                    Toast.makeText(makeAccountActivity.this, "학번 8 자리를 입력해주세요.", Toast.LENGTH_SHORT).show();
             }
         }); //TODO : 이메일 인증하기
 
@@ -136,7 +151,7 @@ public class makeAccountActivity extends LoginActivity {
             @Override
             public void onClick(View v) { // TODO: 인증번호 맞는지 확인하기
                 String inputAuthCode = confirm_num_input.getText().toString();
-                if(inputAuthCode.equals(authCode)){
+                if (inputAuthCode.equals(authCode)) {
                     Toast.makeText(makeAccountActivity.this, "인증 되었습니다.", Toast.LENGTH_SHORT).show();
                     confirm_num_input.setClickable(false);
                     confirm_num_input.setFocusable(false);
@@ -155,8 +170,11 @@ public class makeAccountActivity extends LoginActivity {
                     confrim_button.setTextColor(Color.GRAY);
                     confrim_button.setClickable(false);
                     confrim_button.setFocusable(false);
-                }
-                else
+
+                    count_down.setText("");
+                    count_down.setClickable(false);
+                    count_down.setFocusable(false);
+                } else
                     Toast.makeText(makeAccountActivity.this, "코드를 확인해주세요!", Toast.LENGTH_SHORT).show();
 
             }
@@ -182,16 +200,16 @@ public class makeAccountActivity extends LoginActivity {
 
                 if ((name != null && !name.isEmpty()) && (studentid != null && !studentid.isEmpty())
                         && (confirmnum != null && !confirmnum.isEmpty()) && (pwd != null && !pwd.isEmpty()) && (confirmPswd != null && !confirmPswd.isEmpty())
-                && pswd_confirm.getText()=="일치") {//TODO: 비밀번호, 학번 확인
-                    mAuth.createUserWithEmailAndPassword(studentid+"@inha.edu", pwd)
+                        && pswd_confirm.getText() == "일치") {//TODO: 비밀번호, 학번 확인
+                    mAuth.createUserWithEmailAndPassword(studentid + "@inha.edu", pwd)
                             .addOnCompleteListener(makeAccountActivity.this, new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
-                                        FirebaseUser user =mAuth.getCurrentUser();
+                                        FirebaseUser user = mAuth.getCurrentUser();
                                         Map<String, Object> userMap = new HashMap<>(); //firestore 사용
                                         //userMap.put(FirebaseID.documentID, userMap.getUid()); //사용자 관리하기 위해
-                                        userMap.put(FirebaseID.uID,user.getUid());
+                                        userMap.put(FirebaseID.uID, user.getUid());
                                         userMap.put(FirebaseID.name, name);
                                         userMap.put(FirebaseID.studentID, studentid);
                                         userMap.put(FirebaseID.password, pwd);
@@ -199,7 +217,7 @@ public class makeAccountActivity extends LoginActivity {
                                         mStore.collection(FirebaseID.user)
                                                 .document(user.getUid())
                                                 .set(userMap, SetOptions.merge());//덮어쓰기(추가)
-                                                 finish();
+                                        finish();
                                     } else {
                                         alert_messege.startAnimation(alertMessegeAnim);
                                     }
@@ -208,80 +226,6 @@ public class makeAccountActivity extends LoginActivity {
                 }
             }
         });
-    }
-
-    public void countDown(String time) {
-        long conversionTime = 0;
-
-        // 1000 단위가 1초
-        // 60000 단위가 1분
-        // 60000 * 3600 = 1시간
-
-        String getMin = time.substring(0, 2);   // 1, 2
-        String getSecond = time.substring(2, 4);    // 3, 4
-
-        // "00"이 아니고, 시,분,초의 첫번째 자리가 0 이면 제거
-        if (getMin.substring(0, 1) == "0") {
-            getMin = getMin.substring(1, 2);
-        }
-
-        if (getSecond.substring(0, 1) == "0") {
-            getSecond = getSecond.substring(1, 2);
-        }
-
-        // 변환시간(string->long)
-        conversionTime = Long.valueOf(getMin) * 60 * 1000 + Long.valueOf(getSecond) * 1000;
-
-        // 첫번쨰 인자 : 원하는 시간 (예를들어 30초면 30 x 1000(주기))
-        // 두번쨰 인자 : 주기( 1000 = 1초) = 시간간격
-        new CountDownTimer(conversionTime, 1000) {
-
-            // 특정 시간마다 뷰 변경(타이머가 종료될 때까지 동작)
-            public void onTick(long millisUntilFinished) {
-                // 분단위
-                long getMin = millisUntilFinished - (millisUntilFinished / (60 * 60 * 1000));
-                String min = String.valueOf(getMin / (60 * 1000)); // 몫
-
-                // 초단위
-                String second = String.valueOf((getMin % (60 * 1000)) / 1000); // 나머지
-
-                // 밀리세컨드 단위
-                //String millis = String.valueOf((getMin % (60 * 1000)) % 1000); // 몫
-
-                // 분이 한자리면 0을 붙인다
-                if (min.length() == 1) {
-                    min = "0" + min;
-                }
-
-                // 초가 한자리면 0을 붙인다
-                if (second.length() == 1) {
-                    second = "0" + second;
-                }
-
-                count_view.setText(min + ":" + second);
-            }
-
-            // 제한시간 종료시
-            public void onFinish() {  // TODO : 타이머가 모두 종료될때 어떤 이벤트를 진행할지
-                count_view.setText("종료");
-                new AlertDialog.Builder(makeAccountActivity.this)
-                        .setMessage("시간이 초과되었습니다")
-                        .setTitle("회원가입을 재시도해주세요")
-                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Intent intent = new Intent(makeAccountActivity.this,makeAccountActivity.class);
-                                startActivity(intent);
-                            }
-                        })
-//                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialog, int which) {
-//                            }
-//                        })
-                        .show();
-            }
-        }.start();
     }
 
     private String createEmailCode() { //이메일 인증코드 생성
