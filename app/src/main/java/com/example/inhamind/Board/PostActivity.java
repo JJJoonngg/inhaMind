44cc36bcfdce17cb06eea18efca99992f3a9e177
JJong <whdtls3878@naver.com>
2020-03-03 01:39:23 +0900

    PostActivity finish 기능 추가

--- app/src/main/java/com/example/inhamind/Board/PostActivity.java
+++ app/src/main/java/com/example/inhamind/Board/PostActivity.java
@@ -1,16 +1,39 @@
 package com.example.inhamind.Board;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.inhamind.R;
 
-public class PostActivity extends AppCompatActivity {
+public class PostActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText mTitle, mContents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        findViewById(R.id.done_button).setOnClickListener(this);
+        findViewById(R.id.close_button).setOnClickListener(this);
+
+        mTitle = findViewById(R.id.post_title);
+        mContents = findViewById(R.id.post_contents);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.close_button:
+                this.finish();
+                break;
+
+            case R.id.done_button:
+                break;
+        }
+
     }
 }
