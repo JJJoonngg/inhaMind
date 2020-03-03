332dbf064aca8307c35a8ce7d1867610402d30a9
JJong <whdtls3878@naver.com>
2020-03-03 18:27:25 +0900

    Post작성 후 저장 기능 구현

--- app/src/main/java/com/example/inhamind/Board/PostActivity.java
+++ app/src/main/java/com/example/inhamind/Board/PostActivity.java
@@ -4,13 +4,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.inhamind.FirebaseID;
 import com.example.inhamind.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PostActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+    private FirebaseFirestore mStore = FirebaseFirestore.getInstance();
+
     private EditText mTitle, mContents;
+    private String studentID;
+    private String mUid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +40,24 @@ public class PostActivity extends AppCompatActivity implements View.OnClickListe
 
         mTitle = findViewById(R.id.post_title);
         mContents = findViewById(R.id.post_contents);
+
+        //현재 user의 학번 불러오기
+        if (mUser != null) {
+            mUid = mUser.getUid();
+            mStore.collection(FirebaseID.user).document(mUser.getUid())
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                if (task.getResult() != null) {
+                                    studentID = (String) task.getResult().get(FirebaseID.studentID);
+                                }
+                            }
+
+                        }
+                    });
+        }
     }
 
     @Override
@@ -32,6 +68,17 @@ public class PostActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.done_button:
+                if (mUser != null) {
+                    String postId = mStore.collection(FirebaseID.post).document().getId();
+                    Map<String, Object> data = new HashMap<>();
+                    data.put(FirebaseID.documnetID, mUser.getUid());
+                    data.put(FirebaseID.studentID, studentID);
+                    data.put(FirebaseID.title, mTitle.getText().toString());
+                    data.put(FirebaseID.contents, mContents.getText().toString());
+                    data.put(FirebaseID.timestamp, FieldValue.serverTimestamp());
+                    mStore.collection(FirebaseID.post).document(postId).set(data, SetOptions.merge());
+                    finish();
+                }
                 break;
         }
 
