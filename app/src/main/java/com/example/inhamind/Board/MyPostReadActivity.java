5e5de47a12a7da6084a1f0ddb3f8913457804803
JJong <whdtls3878@naver.com>
2020-03-12 02:51:05 +0900

    Post 관련 추가
    내 글 클릭 시 들어갈 경우 팝업메뉴 추가 -> 수정 & 삭제 위하여
    수정 기능 -> 클릭시 수정 가능하게 해둠 -> 아직 DB 적용은 x
    	  -> 취소시 원래의 글 상태로 되돌려둠

--- app/src/main/java/com/example/inhamind/Board/MyPostReadActivity.java
+++ app/src/main/java/com/example/inhamind/Board/MyPostReadActivity.java
@@ -3,8 +3,12 @@ package com.example.inhamind.Board;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,12 +20,13 @@ import com.google.firebase.firestore.ServerTimestamp;
 
 public class MyPostReadActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView postTitle, postContents, postStatus;
+    private EditText postTitle, postContents, postStatus;
     private String title, contents, studentID, status;
     @ServerTimestamp
     private Timestamp timestamp;
     private Intent intent;
     private Post post;
+    private Button doneBtn, cancelBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +45,10 @@ public class MyPostReadActivity extends AppCompatActivity implements View.OnClic
         postTitle = findViewById(R.id.post_title);
         postContents = findViewById(R.id.post_contents);
         postStatus = findViewById(R.id.post_status);
+        doneBtn = findViewById(R.id.done);
+        doneBtn.setOnClickListener(this);
+        cancelBtn = findViewById(R.id.cancel);
+        cancelBtn.setOnClickListener(this);
 
         postTitle.setText("제목 : " + title);
         postContents.setText(contents);
@@ -57,17 +66,62 @@ public class MyPostReadActivity extends AppCompatActivity implements View.OnClic
 
         findViewById(R.id.close_button).setOnClickListener(this);
         findViewById(R.id.option_button).setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.close_button:
                 this.finish();
                 break;
             case R.id.option_button:
+                PopupMenu popupMenu = new PopupMenu(MyPostReadActivity.this, view);
+                getMenuInflater().inflate(R.menu.popup, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(listener);
+                popupMenu.show();
+                break;
+
+            case R.id.cancel:
+                postTitle.setText("제목 : " + title);
+                postContents.setText(contents);
+                doneBtn.setVisibility(View.INVISIBLE);
+                doneBtn.setFocusable(false);
+                doneBtn.setClickable(false);
+                cancelBtn.setVisibility(View.INVISIBLE);
+                cancelBtn.setFocusable(false);
+                cancelBtn.setClickable(false);
+                postTitle.setFocusable(false);
+                postTitle.setClickable(false);
+                postContents.setFocusable(false);
+                postContents.setFocusable(false);
+                break;
+            case R.id.done:
                 break;
         }
-
     }
+
+    PopupMenu.OnMenuItemClickListener listener = new PopupMenu.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.modify:
+                    doneBtn.setVisibility(View.VISIBLE);
+                    doneBtn.setClickable(true);
+                    doneBtn.setFocusable(true);
+                    cancelBtn.setVisibility(View.VISIBLE);
+                    cancelBtn.setFocusable(true);
+                    cancelBtn.setClickable(true);
+                    postTitle.setFocusableInTouchMode(true);
+                    postTitle.requestFocus();
+                    postContents.setFocusableInTouchMode(true);
+                    postContents.requestFocus();
+                    break;
+                case R.id.delete:
+                    Toast.makeText(MyPostReadActivity.this, "삭제", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return false;
+        }
+    };
 }
