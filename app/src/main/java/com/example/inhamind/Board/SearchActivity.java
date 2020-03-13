2224bd0c874b576767276a82448db6574ec16306
JJong <whdtls3878@naver.com>
2020-03-13 16:05:59 +0900

    contents 최소글자 추가

--- app/src/main/java/com/example/inhamind/Board/SearchActivity.java
+++ app/src/main/java/com/example/inhamind/Board/SearchActivity.java
@@ -3,6 +3,7 @@ package com.example.inhamind.Board;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -19,6 +20,7 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     private InitialStateFragment initialStateFragment = new InitialStateFragment();
 
     private EditText mContents;
+    private String contents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +47,14 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
             case R.id.search_button:
                 FragmentTransaction transaction = fragmentManager.beginTransaction();
                 Fragment fragment = new SearhResultFragment();
-                Bundle bundle = new Bundle(1);
-                bundle.putString(DataName.data, mContents.getText().toString());
-                fragment.setArguments(bundle);
-                transaction.replace(R.id.frameLayout, fragment).commitAllowingStateLoss();
+                if (contents.length() < 2) {
+                    Toast.makeText(getApplicationContext(), "내용을 두 글자 이상 입력해주세요!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Bundle bundle = new Bundle(1);
+                    bundle.putString(DataName.data, contents);
+                    fragment.setArguments(bundle);
+                    transaction.replace(R.id.frameLayout, fragment).commitAllowingStateLoss();
+                }
                 break;
         }
     }
--- app/src/main/java/com/example/inhamind/Board/SearhResultFragment.java
+++ app/src/main/java/com/example/inhamind/Board/SearhResultFragment.java
@@ -8,19 +8,22 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.inhamind.Adapters.PostAdapters;
 import com.example.inhamind.Models.DataName;
 import com.example.inhamind.R;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SearhResultFragment extends Fragment {
 
+    private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
     private RecyclerView mPostRecylerView;
+    private PostAdapters postAdapters;
     private String contents;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_searh_result, container, false);
         contents = getArguments().getString(DataName.data);
-
 
         return view;
     }
