f3a4e1a281b70452df9e35ab1d22eb02aff73407
JJong <whdtls3878@naver.com>
2020-03-14 22:12:43 +0900

    Board 관련 수정
    onEvent로 불러와서 실시간으로 업데이트 되는 것을
    새로고침 해야지만 불러오게함 ->  새글이 등록되어도 원래 보던 창 유지 가능

--- app/src/main/java/com/example/inhamind/Board/BoardFragment.java
+++ app/src/main/java/com/example/inhamind/Board/BoardFragment.java
@@ -17,11 +17,11 @@ import com.example.inhamind.Common.FirebaseID;
 import com.example.inhamind.Common.FloatingButtonFragment;
 import com.example.inhamind.Models.Post;
 import com.example.inhamind.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -51,25 +51,28 @@ public class BoardFragment extends Fragment {
         mDatas = new ArrayList<>();
         mStore.collection(FirebaseID.post)
                 .orderBy(FirebaseID.timestamp, Query.Direction.DESCENDING)
-                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
-                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                        if (queryDocumentSnapshots != null) {
-                            mDatas.clear();
-                            for (DocumentSnapshot snap : queryDocumentSnapshots.getDocuments()) {
-                                Map<String, Object> shot = snap.getData();
-                                String documentID = String.valueOf(shot.get(FirebaseID.documnetID));
-                                String postID = String.valueOf(shot.get(FirebaseID.postID));
-                                String title = String.valueOf(shot.get(FirebaseID.title));
-                                String contents = String.valueOf(shot.get(FirebaseID.contents));
-                                String studentID = String.valueOf(shot.get(FirebaseID.studentID));
-                                String status = String.valueOf(shot.get(FirebaseID.status));
-                                Timestamp timestamp = (Timestamp) shot.get(FirebaseID.timestamp);
-                                Post data = new Post(documentID, postID, title, contents, studentID, status, timestamp);
-                                mDatas.add(data);
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if (task.getResult() != null) {
+                                mDatas.clear();
+                                for (DocumentSnapshot snap : task.getResult()) {
+                                    Map<String, Object> shot = snap.getData();
+                                    String documentID = String.valueOf(shot.get(FirebaseID.documnetID));
+                                    String postID = String.valueOf(shot.get(FirebaseID.postID));
+                                    String title = String.valueOf(shot.get(FirebaseID.title));
+                                    String contents = String.valueOf(shot.get(FirebaseID.contents));
+                                    String studentID = String.valueOf(shot.get(FirebaseID.studentID));
+                                    String status = String.valueOf(shot.get(FirebaseID.status));
+                                    Timestamp timestamp = (Timestamp) shot.get(FirebaseID.timestamp);
+                                    Post data = new Post(documentID, postID, title, contents, studentID, status, timestamp);
+                                    mDatas.add(data);
+                                }
+                                mAdapters = new PostAdapters(getContext(), mDatas);
+                                mPostRecylerView.setAdapter(mAdapters);
                             }
-                            mAdapters = new PostAdapters(getContext(), mDatas);
-                            mPostRecylerView.setAdapter(mAdapters);
                         }
                     }
                 });
@@ -79,32 +82,37 @@ public class BoardFragment extends Fragment {
             public void onRefresh() {
                 mStore.collection(FirebaseID.post)
                         .orderBy(FirebaseID.timestamp, Query.Direction.DESCENDING)
-                        .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                             @Override
-                            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                                if (queryDocumentSnapshots != null) {
-                                    mDatas.clear();
-                                    for (DocumentSnapshot snap : queryDocumentSnapshots.getDocuments()) {
-                                        Map<String, Object> shot = snap.getData();
-                                        String documentID = String.valueOf(shot.get(FirebaseID.documnetID));
-                                        String postID = String.valueOf(shot.get(FirebaseID.postID));
-                                        String title = String.valueOf(shot.get(FirebaseID.title));
-                                        String contents = String.valueOf(shot.get(FirebaseID.contents));
-                                        String studentID = String.valueOf(shot.get(FirebaseID.studentID));
-                                        String status = String.valueOf(shot.get(FirebaseID.status));
-                                        Timestamp timestamp = (Timestamp) shot.get(FirebaseID.timestamp);
-                                        Post data = new Post(documentID, postID, title, contents, studentID, status, timestamp);
-                                        mDatas.add(data);
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    if (task.getResult() != null) {
+                                        mDatas.clear();
+                                        for (DocumentSnapshot snap : task.getResult()) {
+                                            Map<String, Object> shot = snap.getData();
+                                            String documentID = String.valueOf(shot.get(FirebaseID.documnetID));
+                                            String postID = String.valueOf(shot.get(FirebaseID.postID));
+                                            String title = String.valueOf(shot.get(FirebaseID.title));
+                                            String contents = String.valueOf(shot.get(FirebaseID.contents));
+                                            String studentID = String.valueOf(shot.get(FirebaseID.studentID));
+                                            String status = String.valueOf(shot.get(FirebaseID.status));
+                                            Timestamp timestamp = (Timestamp) shot.get(FirebaseID.timestamp);
+                                            Post data = new Post(documentID, postID, title, contents, studentID, status, timestamp);
+                                            mDatas.add(data);
+                                        }
+                                        mAdapters = new PostAdapters(getContext(), mDatas);
+                                        mPostRecylerView.setAdapter(mAdapters);
                                     }
-                                    mAdapters = new PostAdapters(getContext(), mDatas);
-                                    mPostRecylerView.setAdapter(mAdapters);
                                 }
                             }
                         });
+
                 swipeRefreshLayout.setRefreshing(false);
             }
         });
 
         return view;
     }
+
 }
