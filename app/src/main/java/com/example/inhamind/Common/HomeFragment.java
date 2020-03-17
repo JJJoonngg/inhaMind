28c1fdfd50527e2d885efcc7f1017821d8d24b92
JJong <whdtls3878@naver.com>
2020-03-17 21:56:39 +0900

    Home Fragment swipe 하여 새로고침 기능 추가

--- app/src/main/java/com/example/inhamind/Common/HomeFragment.java
+++ app/src/main/java/com/example/inhamind/Common/HomeFragment.java
@@ -12,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.inhamind.Account.MyPageActivty;
 import com.example.inhamind.Adapters.MainMyPostAdapters;
@@ -48,6 +49,8 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     private MainPostAdapters allAdapters;
     private MainMyPostAdapters myAllAdapters;
     private MainNoticeAdapters noticeAdapters;
+    private SwipeRefreshLayout swipeRefreshLayout;
+
     private List<Post> allPostDatas, postDatas;
     private List<Notice> noticeDatas;
 
@@ -89,6 +92,129 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                         }
                     });
         }
+
+        swipeRefreshLayout = view.findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                noticeDatas = new ArrayList<>();
+                mStore.collection(FirebaseID.notice)
+                        .orderBy(FirebaseID.timestamp, Query.Direction.DESCENDING)
+                        .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                            @Override
+                            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                                if (queryDocumentSnapshots != null) {
+                                    noticeDatas.clear();
+                                    int cnt = 0;
+                                    for (DocumentSnapshot snap : queryDocumentSnapshots.getDocuments()) {
+                                        Map<String, Object> shot = snap.getData();
+                                        String documentID = String.valueOf(shot.get(FirebaseID.documnetID));
+                                        String noticeID = String.valueOf(shot.get(FirebaseID.noticeID));
+                                        String title = String.valueOf(shot.get(FirebaseID.title));
+                                        String contents = String.valueOf(shot.get(FirebaseID.contents));
+                                        String name = String.valueOf(shot.get(FirebaseID.name));
+                                        Timestamp timestamp = (Timestamp) shot.get(FirebaseID.timestamp);
+                                        Notice data = new Notice(documentID, noticeID, name, title, contents, timestamp);
+                                        noticeDatas.add(data);
+                                        cnt++;
+                                        break;
+                                    }
+                                    if (cnt == 0) {
+                                        Notice data = new Notice(null, null, null, null, null, null);
+                                        noticeDatas.add(data);
+                                    }
+                                    noticeAdapters = new MainNoticeAdapters(noticeDatas, getContext());
+                                    noticeRecylerView.setAdapter(noticeAdapters);
+                                }
+                            }
+                        });
+
+                allPostDatas = new ArrayList<>();
+                mStore.collection(FirebaseID.post)
+                        .orderBy(FirebaseID.timestamp, Query.Direction.DESCENDING)
+                        .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                            @Override
+                            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                                if (queryDocumentSnapshots != null) {
+                                    allPostDatas.clear();
+                                    int cnt = 0;
+                                    for (DocumentSnapshot snap : queryDocumentSnapshots.getDocuments()) {
+                                        if (cnt == 3) break;
+                                        Map<String, Object> shot = snap.getData();
+                                        String documentID = String.valueOf(shot.get(FirebaseID.documnetID));
+                                        String postID = String.valueOf(shot.get(FirebaseID.postID));
+                                        String title = String.valueOf(shot.get(FirebaseID.title));
+                                        String contents = String.valueOf(shot.get(FirebaseID.contents));
+                                        String studentID = String.valueOf(shot.get(FirebaseID.studentID));
+                                        String status = String.valueOf(shot.get(FirebaseID.status));
+                                        Timestamp timestamp = (Timestamp) shot.get(FirebaseID.timestamp);
+                                        Post data = new Post(documentID, postID, title, contents, studentID, status, timestamp);
+                                        allPostDatas.add(data);
+                                        cnt++;
+                                    }
+                                    if (cnt < 3) {
+                                        while (true) {
+                                            if (cnt == 3) break;
+                                            Post data = new Post(null, null, null, null, null, null, null);
+                                            allPostDatas.add(data);
+                                            cnt++;
+                                        }
+                                    }
+
+                                    allAdapters = new MainPostAdapters(allPostDatas, getContext());
+                                    allPostRecylerView.setAdapter(allAdapters);
+                                }
+                            }
+                        });
+
+                if (mUser != null) {
+                    postDatas = new ArrayList<>();
+                    for (int i = 0; i < 3; i++) {
+                        Post data = new Post(null, null, null, null, null, null, null);
+                        postDatas.add(data);
+                        myAllAdapters = new MainMyPostAdapters(postDatas, getContext());
+                        myPostRecylerView.setAdapter(myAllAdapters);
+                    }
+                    mStore.collection(FirebaseID.post)
+                            .whereEqualTo(FirebaseID.documnetID, mUser.getUid())
+                            .orderBy(FirebaseID.timestamp, Query.Direction.DESCENDING)
+                            .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                @Override
+                                public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                                    if (queryDocumentSnapshots != null) {
+                                        postDatas.clear();
+                                        int cnt = 0;
+                                        for (DocumentSnapshot snap : queryDocumentSnapshots.getDocuments()) {
+                                            if (cnt == 3) break;
+                                            Map<String, Object> shot = snap.getData();
+                                            String documentID = String.valueOf(shot.get(FirebaseID.documnetID));
+                                            String postID = String.valueOf(shot.get(FirebaseID.postID));
+                                            String title = String.valueOf(shot.get(FirebaseID.title));
+                                            String contents = String.valueOf(shot.get(FirebaseID.contents));
+                                            String studentID = String.valueOf(shot.get(FirebaseID.studentID));
+                                            String status = String.valueOf(shot.get(FirebaseID.status));
+                                            Timestamp timestamp = (Timestamp) shot.get(FirebaseID.timestamp);
+                                            Post data = new Post(documentID, postID, title, contents, studentID, status, timestamp);
+                                            postDatas.add(data);
+                                            cnt++;
+                                        }
+                                        if (cnt < 3) {
+                                            while (true) {
+                                                if (cnt == 3) break;
+                                                Post data = new Post(null, null, null, null, null, null, null);
+                                                postDatas.add(data);
+                                                cnt++;
+                                            }
+                                        }
+                                        myAllAdapters = new MainMyPostAdapters(postDatas, getContext());
+                                        myPostRecylerView.setAdapter(myAllAdapters);
+                                    }
+                                }
+                            });
+                }
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
 
         return view;
     }
@@ -216,9 +342,9 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.main_button:
-                Toast.makeText(context,"환영합니다.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "환영합니다.", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.write_button:
                 startActivity(new Intent(context, PostWriteActivity.class));
