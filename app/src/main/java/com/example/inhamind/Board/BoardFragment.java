50a5861c9f4ac3ef176bbd4af8a8da493428e1c1
di6223 <di6223@naver.com>
2020-03-20 15:56:57 +0900

    이용약관 추가 및 Jong과 merge 리사이클러뷰 접고펴기 기능 종속성 추가

--- app/src/main/java/com/example/inhamind/Board/BoardFragment.java
+++ app/src/main/java/com/example/inhamind/Board/BoardFragment.java
@@ -9,18 +9,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.inhamind.Adapters.PostAdapters;
 import com.example.inhamind.Common.FirebaseID;
 import com.example.inhamind.Common.FloatingButtonFragment;
 import com.example.inhamind.Models.Post;
 import com.example.inhamind.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -28,9 +30,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class BoardFragment extends Fragment {
+public class BoardFragment extends Fragment implements View.OnClickListener {
 
     private FirebaseFirestore mStore = FirebaseFirestore.getInstance();
+    private SwipeRefreshLayout swipeRefreshLayout;
     private RecyclerView mPostRecylerView;
     private PostAdapters mAdapters;
     private List<Post> mDatas;
@@ -44,32 +47,84 @@ public class BoardFragment extends Fragment {
         transaction.replace(R.id.fab_layout, fabFragment).commitAllowingStateLoss();
 
         mPostRecylerView = view.findViewById(R.id.board_recyclerview);
+        swipeRefreshLayout = view.findViewById(R.id.swipe_refresh_layout);
+
+        view.findViewById(R.id.top_button).setOnClickListener(this);
 
         mDatas = new ArrayList<>();
         mStore.collection(FirebaseID.post)
                 .orderBy(FirebaseID.timestamp, Query.Direction.DESCENDING)
-                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
-                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                        if (queryDocumentSnapshots != null) {
-                            mDatas.clear();
-                            for (DocumentSnapshot snap : queryDocumentSnapshots.getDocuments()) {
-                                Map<String, Object> shot = snap.getData();
-                                String documentID = String.valueOf(shot.get(FirebaseID.documnetID));
-                                String postID = String.valueOf(shot.get(FirebaseID.postID));
-                                String title = String.valueOf(shot.get(FirebaseID.title));
-                                String contents = String.valueOf(shot.get(FirebaseID.contents));
-                                String studentID = String.valueOf(shot.get(FirebaseID.studentID));
-                                String status = String.valueOf(shot.get(FirebaseID.status));
-                                Timestamp timestamp = (Timestamp) shot.get(FirebaseID.timestamp);
-                                Post data = new Post(documentID, postID, title, contents, studentID, status, timestamp);
-                                mDatas.add(data);
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if (task.getResult() != null) {
+                                mDatas.clear();
+                                for (DocumentSnapshot snap : task.getResult()) {
+                                    Map<String, Object> shot = snap.getData();
+                                    String documentID = String.valueOf(shot.get(FirebaseID.documnetID));
+                                    String postID = String.valueOf(shot.get(FirebaseID.postID));
+                                    String title = String.valueOf(shot.get(FirebaseID.title));
+                                    String contents = String.valueOf(shot.get(FirebaseID.contents));
+                                    String studentID = String.valueOf(shot.get(FirebaseID.studentID));
+                                    String status = String.valueOf(shot.get(FirebaseID.status));
+                                    Timestamp timestamp = (Timestamp) shot.get(FirebaseID.timestamp);
+                                    Post data = new Post(documentID, postID, title, contents, studentID, status, timestamp);
+                                    mDatas.add(data);
+                                }
+                                mAdapters = new PostAdapters(getContext(), mDatas);
+                                mPostRecylerView.setAdapter(mAdapters);
                             }
-                            mAdapters = new PostAdapters(getContext(), mDatas);
-                            mPostRecylerView.setAdapter(mAdapters);
                         }
                     }
                 });
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mStore.collection(FirebaseID.post)
+                        .orderBy(FirebaseID.timestamp, Query.Direction.DESCENDING)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    if (task.getResult() != null) {
+                                        mDatas.clear();
+                                        for (DocumentSnapshot snap : task.getResult()) {
+                                            Map<String, Object> shot = snap.getData();
+                                            String documentID = String.valueOf(shot.get(FirebaseID.documnetID));
+                                            String postID = String.valueOf(shot.get(FirebaseID.postID));
+                                            String title = String.valueOf(shot.get(FirebaseID.title));
+                                            String contents = String.valueOf(shot.get(FirebaseID.contents));
+                                            String studentID = String.valueOf(shot.get(FirebaseID.studentID));
+                                            String status = String.valueOf(shot.get(FirebaseID.status));
+                                            Timestamp timestamp = (Timestamp) shot.get(FirebaseID.timestamp);
+                                            Post data = new Post(documentID, postID, title, contents, studentID, status, timestamp);
+                                            mDatas.add(data);
+                                        }
+                                        mAdapters = new PostAdapters(getContext(), mDatas);
+                                        mPostRecylerView.setAdapter(mAdapters);
+                                    }
+                                }
+                            }
+                        });
+
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.top_button:
+                LinearLayoutManager linearLayoutManager = (LinearLayoutManager) mPostRecylerView.getLayoutManager();
+                mPostRecylerView.smoothScrollToPosition(0);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/inhamind/Common/MainActivity.java
+++ app/src/main/java/com/example/inhamind/Common/MainActivity.java
@@ -1,137 +1,38 @@
 package com.example.inhamind.Common;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.example.inhamind.Account.MyPageActivty;
-import com.example.inhamind.Account.ServiceCenter;
 import com.example.inhamind.Board.BoardFragment;
 import com.example.inhamind.Chat.ChattingFragment;
-import com.example.inhamind.Models.DataName;
-import com.example.inhamind.Models.User;
-import com.example.inhamind.Notice.NoticeListActivity;
 import com.example.inhamind.R;
 import com.example.inhamind.Setting.SettingFragment;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout mDrawerLayout;
-
     private FragmentManager fragmentManager = getSupportFragmentManager();
-    private HomeFragment homeFragment = new HomeFragment();
+    private HomeFragment homeFragment;
     private BoardFragment boardFragment = new BoardFragment();
     private SettingFragment settingFragment = new SettingFragment();
     private ChattingFragment chattingFragment = new ChattingFragment();
-    private CustomerServiceFragment customerServiceFragment = new CustomerServiceFragment();
-
-    private FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
-    private FirebaseFirestore mStore = FirebaseFirestore.getInstance();
-    private User user;
-    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        homeFragment = new HomeFragment();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.frameLayout, homeFragment).commitAllowingStateLoss();
+        transaction.replace(R.id.frameLayout, homeFragment).commit();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayShowTitleEnabled(false); // 기존 title 지우기
-        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 만들기
-        actionBar.setHomeAsUpIndicator(R.drawable.baseline_menu_black_24); //뒤로가기 버튼 이미지 지정
-
-        //현재 user의 학번 불러오기
-        if (mUser != null) {
-            mStore.collection(FirebaseID.user).document(mUser.getUid())
-                    .get()
-                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                            if (task.isSuccessful()) {
-                                if (task.getResult() != null) {
-                                    String documentID = (String) task.getResult().get(FirebaseID.documnetID);
-                                    String studentID = (String) task.getResult().get(FirebaseID.studentID);
-                                    String name = (String) task.getResult().get(FirebaseID.name);
-                                    user = new User(documentID, name, studentID);
-                                }
-                            }
-
-                        }
-                    });
-        }
-
-        mDrawerLayout = findViewById(R.id.drawer_layout);
-
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-                menuItem.setChecked(true);
-                mDrawerLayout.closeDrawers();
-
-                switch (menuItem.getItemId()) {
-                    case R.id.myPage:
-                        intent = new Intent(MainActivity.this, MyPageActivty.class);
-                        intent.putExtra(DataName.user, user);
-                        startActivity(intent);
-                        break;
-                    case R.id.notice:
-                        intent = new Intent(MainActivity.this, NoticeListActivity.class);
-                        intent.putExtra(DataName.user, user);
-                        startActivity(intent);
-                        break;
-                    case R.id.customerService:
-                        startActivity(new Intent(MainActivity.this, ServiceCenter.class));
-                        break;
-                }
-                return true;
-            }
-        });
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home: { // 왼쪽 상단 버튼 눌렀을 때
-                mDrawerLayout.openDrawer(GravityCompat.START);
-                return true;
-            }
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
-            mDrawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 
     class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
@@ -140,16 +41,16 @@ public class MainActivity extends AppCompatActivity {
             FragmentTransaction transaction = fragmentManager.beginTransaction();
             switch (menuItem.getItemId()) {
                 case R.id.home:
-                    transaction.replace(R.id.frameLayout, homeFragment).commitAllowingStateLoss();
+                    transaction.replace(R.id.frameLayout, homeFragment).commit();
                     break;
                 case R.id.board:
-                    transaction.replace(R.id.frameLayout, boardFragment).commitAllowingStateLoss();
+                    transaction.replace(R.id.frameLayout, boardFragment).commit();
                     break;
                 case R.id.chatting:
-                    transaction.replace(R.id.frameLayout, chattingFragment).commitAllowingStateLoss();
+                    transaction.replace(R.id.frameLayout, chattingFragment).commit();
                     break;
                 case R.id.setting:
-                    transaction.replace(R.id.frameLayout, settingFragment).commitAllowingStateLoss();
+                    transaction.replace(R.id.frameLayout, settingFragment).commit();
                     break;
             }
             return true;
