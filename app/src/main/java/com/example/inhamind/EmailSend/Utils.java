a2d6ecb544ab43011b3e40545e9678fd375b29e3
JJong <whdtls3878@gmail.com>
2020-04-20 00:42:41 +0900

    remove EmailSend Directory

--- app/src/main/java/com/example/inhamind/EmailSend/Utils.java
+++
@@ -1,9 +0,0 @@
-package com.example.inhamind.EmailSend;
-
-public class Utils {
-    //This is your from email
-    public static final String EMAIL = "inhamindteam@gmail.com";
-
-    //This is your from email password
-    public static final String PASSWORD = "12164715!ab";
-}
